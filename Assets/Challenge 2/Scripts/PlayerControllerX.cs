7890ae3c4428ea8be29668cc4f8980ff2b9e3d34
joelise <jo.soyke@gmail.com>
2025-07-08 18:02:43 +1000

    Challenge 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float cooldown = 3f;
+    public float timeSinceCooldown;
+
+    private void Start()
+    {
+        timeSinceCooldown = cooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceCooldown += Time.deltaTime;
+        
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timeSinceCooldown >= cooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeSinceCooldown = 0f;
+            
+        }
+    }
+}
